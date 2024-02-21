80fc35a8054f8a47797c577bac4717183d7bd1f6
Cindy Posada <cindypsdi@gmail.com>
2024-02-20 21:19:21 -0600

    feat: initial auth pages setup

--- src/auth/pages/SignInPage.tsx
+++ src/auth/pages/SignInPage.tsx
@@ -1,7 +1,41 @@
-import { Typography } from "@mui/material"
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography, Link } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const SignInPage = () => {
   return (
-    <Typography>SignInPage</Typography>
-  )
-}
+    <AuthLayout title="Login">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField label="Email" placeholder="email" variant="outlined" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField label="Password" placeholder="password" variant="outlined" fullWidth />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/signUp">
+              Create account
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
--- src/auth/pages/SignUpPage.tsx
+++ src/auth/pages/SignUpPage.tsx
@@ -1,7 +1,49 @@
-import { Typography } from "@mui/material"
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography, Link } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const SignUpPage = () => {
   return (
-    <Typography variant="h1">SignUpPage</Typography>
-  )
-}
+    <AuthLayout title="Create account">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField label="Name" type="text" placeholder="name" variant="outlined" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="password"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <Button variant="contained" fullWidth>
+              Create
+            </Button>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>Already have an account?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/signIn">
+              Sign in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
